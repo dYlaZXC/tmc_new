07377b576cdc0580e108cf7b57399f071e3c9e99
ZarickThustrovich <skyline123skyline@gmail.com>
2022-04-03 16:57:55 +0600

    thx

--- tmc_new/views.py
+++ tmc_new/views.py
@@ -2,6 +2,7 @@ from distutils.ccompiler import new_compiler
 from multiprocessing.sharedctypes import Value
 from posixpath import split
 from tabnanny import check
+from tkinter.tix import S_REGION
 from django.http import JsonResponse, HttpResponse
 from django.shortcuts import render, redirect
 from rest_framework.views import APIView
@@ -23,8 +24,27 @@ from django.template.loader import render_to_string
 
 class Card(APIView):
     def get(self, request, *args, **kwargs):
-        
-        return render(request, 'card.html') 
+        # CONTEXT
+        s_categories = SCategory.objects.all()
+        s_ratios = SRatio.objects.all()
+        s_regions = SRegion.objects.all()
+        s_villages = SVillage.objects.all()
+        s_streets = SStreet.objects.all()
+        s_countries = SCountry.objects.all()
+        s_riskgroups = SRiskGroup.objects.all()
+        s_conditions = SCondition.objects.all()
+        # END CONTEXT
+        return render(request, 'card.html',
+        {
+            's_categories': s_categories,
+            's_ratios': s_ratios,
+            's_regions': s_regions,
+            's_villages': s_villages,
+            's_streets': s_streets,
+            's_countries': s_countries,
+            's_riskgroups': s_riskgroups,
+            's_conditions': s_conditions,
+        }) 
 
 
 @api_view(["GET"])
