9650ab78c646dd68a5612e3000aaea586dc9a67a
ZarickThustrovich <skyline123skyline@gmail.com>
2022-04-13 17:06:33 +0600

    13 04 2022   17 06

--- tmc_new/urls.py
+++ tmc_new/urls.py
@@ -31,5 +31,11 @@ urlpatterns = [
 
     #ЧЕК ЛИСТЫ
     path('card/checklist/save/<int:id>', CheckListSaveView.as_view(), name='checklist-save'),
-    path('card/checklists/<int:id>', CheckListJournalView.as_view(), name='checklists-journal'),
+    path('card/checklists/journal/<int:id>', CheckListJournalView.as_view(), name='checklist-journal'),
+    path('card/checklists/calls/<int:id>', CheckListCallsView.as_view(), name='checklist-calls'),
+    # path('card/checklists/patients/<int:id>', CheckListPatientsView.as_view(), name='checklist-patients'),
+
+    #TEST
+    # path('card/test/', set_users, name='test'),
+    # path('card/test/get', parse_users, name='test-get')
 ]
